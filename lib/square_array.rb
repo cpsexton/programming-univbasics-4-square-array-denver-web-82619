d15fb2f770767030980e984a327c56aff6aa793f
Christopher Sexton <cpsexton@gmail.com>
2019-08-04 22:42:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -11,8 +11,8 @@ def square_array(array)
   end
 end
 numbers = [1,2,3]
-puts square_array(numbers)
+square_array(numbers)
 
 new_numbers = [9,10,16,25]
-puts square_array(new_numbers)
+square_array(new_numbers)
 
