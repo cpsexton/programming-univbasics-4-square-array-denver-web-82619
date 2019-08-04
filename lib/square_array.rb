0a5c1cd16b2aa156dc8dd106b5b4e067094324ae
Christopher Sexton <cpsexton@gmail.com>
2019-08-04 22:09:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,9 @@ def square_array(numbers)
   counter = 0
   
   while counter < numbers.length do
-    sum = numbers(counter)
+    sum = numbers[counter]
     Math.sqrt(sum)
     
-    puts sum
     counter += 1
 
   end
