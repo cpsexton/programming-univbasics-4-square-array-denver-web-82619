d5d8a4c8d69e0e77c4c970245c3d95f44921aa2b
Christopher Sexton <cpsexton@gmail.com>
2019-08-04 23:26:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,9 +8,9 @@ def square_array(array)
     counter += 1
     end
 end
-numbers = [1,2,3]
+#numbers = [1,2,3]
 square_array(numbers)
 
-new_numbers = [9,10,16,25]
+#new_numbers = [9,10,16,25]
 square_array(new_numbers)
 
