43ed4dc15e149ba9364cb6d2c2605485bdc8ab09
Christopher Sexton <cpsexton@gmail.com>
2019-08-05 00:16:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,10 @@ def square_array(array)
   counter = 0
   x = []
   while array[counter] do
-  
     x.push(array[counter]**2)
     counter += 1
   end
- p x
+  p x
 end
 numbers = [1,2,3]
 square_array(numbers)
